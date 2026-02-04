cd89a995a460f57f9d0c1666d484b12beae4a6f1
Hyan Ventura <hyanventura12@gmail.com>
2026-02-03 23:14:47 -0300

    feat: adiciona logo no header

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,17 @@
 export const Header = () => {
     return (
         <header className="header">
-            <h3 className="title">CALOTE FC</h3>
+            <h3 className="title" style={{ display: 'flex', justifyContent:"center", alignItems: "center", padding: '10px' }}>
+            <img 
+                style={{ 
+                    height: 100, 
+                    width: 100,
+                }} 
+                src="./public/caloteLogo.png" 
+                alt="Calote FC Logo"
+            />
+                CALOTE FC 
+            </h3>
         </header>
     )
 }
